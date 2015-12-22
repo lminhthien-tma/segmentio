ba55ac4010d27a7bec40138201cf9d049df13453
Thien Le <lminhthien@tma.com.vn>
2015-12-22 14:30:57 +0700

    upload source code

---
+++ lib/segmentio/analytics.rb
@@ -0,0 +1,31 @@
+require 'segmentio/analytics/defaults'
+require 'segmentio/analytics/utils'
+require 'segmentio/analytics/version'
+require 'segmentio/analytics/client'
+require 'segmentio/analytics/worker'
+require 'segmentio/analytics/request'
+require 'segmentio/analytics/response'
+require 'segmentio/analytics/logging'
+
+module Segmentio
+  class Analytics
+    def initialize options = {}
+      Request.stub = options[:stub]
+      @client = Segmentio::Analytics::Client.new options
+    end
+
+    def method_missing message, *args, &block
+      if @client.respond_to? message
+        @client.send message, *args, &block
+      else
+        super
+      end
+    end
+
+    def respond_to? method_name, include_private = false
+      @client.respond_to?(method_name) || super
+    end
+
+    include Logging
+  end
+end
---
+++ lib/segmentio/analytics/version.rb
@@ -0,0 +1,5 @@
+module Segmentio
+  class Analytics
+    VERSION = '2.0.13'
+  end
+end
